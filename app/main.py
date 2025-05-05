caba96edc43c4ce0256d9bb3ba65095b2f31238f
Mustafa-Lutaaya <cestmoimuganga@gmx.de>
2025-05-05 18:05:48 +0200

    Database Initialization In Main App
    Retrieved Items From Database By Category In The Main App

--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 
-# Utilities
+# Utilities & CO2 Connection Class
 from datetime import datetime
+from sqlite.sql_lite import CO2
 
 # Initializes FastAPI Web Application
 app = FastAPI() 
+
+# Database Initialization
+berechner_db = CO2("Berechner.db") # Creates an instance of  CO2 class from sqlite.sql_lite.
+table_name = "CO2_Spar" # Stores Table Name
+items = berechner_db.get_data_by_category(table_name) # Retrieves data grouped by category from table
+print(items) 
