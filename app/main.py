26f8e803dbbfd9fabf7175285f06ecf5897c8588
Mustafa-Lutaaya <cestmoimuganga@gmx.de>
2025-05-05 18:38:36 +0200

    Jinja2Templates & Route 4 Home Page
    Added Jinja2Templates Library & a Templates Folder With an index.html then defined the root page using a Get Request and rendered the items to the template.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 # FastAPI Components to build the web app.
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Request, Form  # Imports FastAPI , Request for handling HTTP requests & Form to accept data submitted via POST Requests
+from fastapi.responses import HTMLResponse, RedirectResponse # Specifies that a route returns HTML
+from fastapi.templating import Jinja2Templates  # Imports Jinja2 template support
 
 # Utilities & CO2 Connection Class
 from datetime import datetime
 from sqlite.sql_lite import CO2
+from pathlib import Path # Provides object-oriented file system paths
 
 # Initializes FastAPI Web Application
 app = FastAPI() 
+templates = Jinja2Templates(directory=Path(__file__).parent.parent/"templates")  # Sets up Jinja2Templates for dynamic HTML rendering from templates folder
 
 # Database Initialization
 berechner_db = CO2("Berechner.db") # Creates an instance of  CO2 class from sqlite.sql_lite.
 table_name = "CO2_Spar" # Stores Table Name
 items = berechner_db.get_data_by_category(table_name) # Retrieves data grouped by category from table
-print(items) 
+
+# Route for the homepage ('/') that returns an HTML response displaying items
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    context = {
+        "request": request, # Passes the request for Jinja2 to access
+        "items": items  # Passes items data to be rendered
+    }
+    return templates.TemplateResponse(request, "index.html", context)  # Renders the 'index.html' template with data from the context dictionary
